326b0f3bf3305878acabb89a217995c45bcdba00
Nika Chitanava <chitanava@gmail.com>
2023-04-16 01:57:17 +0400

    separate assets for back and front

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/front.css', 'resources/css/back.css', 
+                'resources/js/back.js', 'resources/js/front.js'
+            ],
             refresh: true,
         }),
     ],
