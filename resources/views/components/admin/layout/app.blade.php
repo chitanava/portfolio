e7cc01a89a99d01966f8d325f40f8671a00a5751
Nika Chitanava <chitanava@gmail.com>
2023-04-21 20:49:27 +0400

    add feature to automatically hide success messages after a set duration

--- resources/views/components/admin/layout/app.blade.php
+++ resources/views/components/admin/layout/app.blade.php
@@ -35,7 +35,22 @@
 
         <main class="w-full max-w-7xl mx-auto px-6 gap-8 mb-8 flex flex-col">
           @if (session('status'))
-            <div class="alert alert-success shadow-sm">
+            <div 
+              x-data="{
+                show: true,
+                _t: undefined,
+                timeout(){
+                  this._t = setTimeout(() => this.show = false, 5000);
+                }
+              }",
+              x-init="
+                timeout();
+              " 
+              x-on:mouseenter="clearTimeout(_t)"
+              x-on:mouseleave="timeout()"
+              x-show="show"
+              x-cloak
+              class="alert alert-success shadow-sm">
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                 <span class="text-sm">{{ session('status') }}</span>
