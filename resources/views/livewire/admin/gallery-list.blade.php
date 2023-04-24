fbed9c8c82a79b81b14dedd32d2f3f786399c4f1
Nika Chitanava <chitanava@gmail.com>
2023-04-24 05:52:00 +0400

    include links in gallery actions menu

--- resources/views/livewire/admin/gallery-list.blade.php
+++ resources/views/livewire/admin/gallery-list.blade.php
@@ -1,15 +1,18 @@
 <div>
     @if ($galleries->isEmpty())
-    <div>No items found in gallery. Please add new content to display.</div>
+    <div>No items found. Please add new content to display.</div>
     @else
     <x-admin.sortable.head class="grid-cols-[2rem_3fr_1fr_1fr]" :cols="['title']" />
+
 
     <x-admin.sortable.list wire:sortable="updateOrder">
         @foreach ($galleries as $gallery)
         <x-admin.sortable.list-item class="grid-cols-[2rem_3fr_1fr_1fr]" wire:sortable.item="{{ $gallery->id }}"
             wire:key="task-{{ $gallery->id }}">
 
-            <div class="justify-self-start">{{ $gallery->title }}</div>
+            <div class="justify-self-start">
+                <a class="hover:underline underline-offset-4" href="{{ route('admin.galleries.show', $gallery->id) }}">{{ $gallery->title }}</a>
+            </div>
 
             <x-slot:active>
                 <input wire:click="active({{ $gallery->id }})" name="active" type="checkbox" class="toggle toggle-sm" @checked($gallery->active) />
@@ -42,6 +45,25 @@
                         </x-admin.icon>
                         Delete
                     </label>
+                </li>
+                <li>
+                    <div class="divider hover:bg-transparent cursor-auto">OR</div>
+                </li>
+                <li>
+                    <a href="{{ route('admin.galleries.albums.create', $gallery->id) }}">
+                        <x-admin.icon size="4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                        </x-admin.icon>
+                        Add album 
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <x-admin.icon size="4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                        </x-admin.icon>
+                        Add image 
+                    </a>
                 </li>
             </x-slot:actions>
         </x-admin.sortable.list-item>
