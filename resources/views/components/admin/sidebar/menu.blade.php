2e7fb92f022253c1f0b6796df1d040a7612ab1e6
Nika Chitanava <chitanava@gmail.com>
2023-04-24 05:26:09 +0400

    update icon for galleries in sidebar menu

--- resources/views/components/admin/sidebar/menu.blade.php
+++ resources/views/components/admin/sidebar/menu.blade.php
@@ -1,7 +1,7 @@
 <ul class="menu menu-compact py-0 px-4 space-y-1">
   <x-admin.sidebar.menu-item :link="route('admin.galleries')" :active="active_link('admin.galleries*')">
     <x-admin.icon stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
     </x-admin.icon>
     Galleries
   </x-admin.sidebar.menu-item>
