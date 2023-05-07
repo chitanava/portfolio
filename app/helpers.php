86b0b573b214338fad52ea98dcbc03d3b41cfa40
Nika Chitanava <chitanava@gmail.com>
2023-05-08 01:38:18 +0400

    add helper to activate gallery in navigation

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 function active_link(string $name): bool
 {
   return request()->routeIs($name);
+}
+
+function active_gallery_link(string $name, string $slug): bool
+{
+  return request()->routeIs($name) && head(Route::current()->parameters())->slug === $slug;
 }
