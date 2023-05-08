4f7d5d0c0adc8c6810692a99b4ebc874f9f9e3f4
Nika Chitanava <chitanava@gmail.com>
2023-05-08 20:45:57 +0400

    retrieve biography from language file

--- resources/views/site/biography.blade.php
+++ resources/views/site/biography.blade.php
@@ -1,5 +1,5 @@
 <x-site.layout.app>
-  <x-slot name="title">Biography</x-slot>
+  <x-slot name="title">{{ __('common.Biography') }}</x-slot>
   
   <div class="md:w-1/2 lg:w-1/3 text-justify trix-content">
     {!! htmlspecialchars_decode($biography->body) !!}
