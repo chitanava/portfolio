c7f1ba2af48c2cebebca2ace8f909647f5fe5d06
Nika Chitanava <chitanava@gmail.com>
2023-05-08 20:48:46 +0400

    refactor site app layout

--- resources/views/components/site/layout/app.blade.php
+++ resources/views/components/site/layout/app.blade.php
@@ -16,6 +16,7 @@
     }
   </style>
   @stack('header-scripts')
+  @if ($settings->custom_css)<style>{!! $settings->custom_css !!}</style>@endif
   <title>@isset($title) {{ $title }} - @endisset {{ config('app.name') }}</title>
 </head>
 
@@ -23,39 +24,14 @@
   <div class="drawer drawer-mobile">
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col relative">
-      <!-- Page content here -->
-      {{-- <label for="my-drawer" class="btn btn-primary drawer-button lg:hidden">Open drawer</label> --}}
-      <main class="py-10 pl-10 pr-10 lg:pl-0">
+      <main class="py-10 pl-10 pr-10 lg:pl-0 space-y-5 lg:space-y-0">
+        <x-site.header/>
         {{ $slot }}
       </main>
     </div>
-    <div class="drawer-side ">
-      <label for="my-drawer" class="drawer-overlay"></label>
-      <aside class="w-80 bg-white">
-        <div class="py-10 pl-10 pr-4 flex flex-col gap-10">
-          <h2 class="font-La-belle-aurore text-5xl">Zautashvili</h2>
-          <nav>
-            <ul>
-              <li>
-                <a href="{{ route('home') }}" class="text-xl font-bold hover:text-gray-900 block py-1 {{ active_link('home') ? 'text-gray-900' : 'text-gray-500' }}">Home</a>
-              </li>
-              <li>
-                <a href="{{ route('biography') }}" class="text-xl font-bold hover:text-gray-900 block py-1 {{ active_link('biography') ? 'text-gray-900' : 'text-gray-500' }}">Biography</a>
-              </li>
-              @foreach ($galleries as $gallery)
-              <li>
-                <a href="{{ route('gallery', $gallery->slug) }}" class="text-xl font-bold hover:text-gray-900 block py-1 {{ active_gallery_link('gallery*', $gallery->slug) ? 'text-gray-900' : 'text-gray-500' }}">{{ $gallery->title }}</a>
-              </li>      
-              @endforeach
-            </ul>
-          </nav>
-
-          {{ $albumDescription ?? '' }}
-
-          <div class="caption"></div>
-        </div>
-      </aside>
-    </div>
+    <x-site.sidebar>
+      {{ $albumDescription ?? '' }}
+    </x-site.sidebar>
   </div>
 
   @livewireScripts
