86857fb2079a0fb6e810f957e164d881d3f4c994
Nika Chitanava <chitanava@gmail.com>
2023-05-08 20:41:30 +0400

    add redirection to site when clicking logo in admin panel

--- resources/views/components/admin/sidebar/index.blade.php
+++ resources/views/components/admin/sidebar/index.blade.php
@@ -1,7 +1,7 @@
 <aside class="bg-base-200 w-80 flex flex-col gap-8">
   <div class="h-16 flex justify-center items-center">
-    <a class="btn btn-ghost lowercase font-normal">
-      <span class="mt-3 font-La-belle-aurore text-4xl">Zautashvili</span>
+    <a href="{{ route('home') }}" target="_blank" class="btn btn-ghost lowercase font-normal">
+      <span class="mt-3 font-La-belle-aurore text-4xl">{{ config('app.name') }}</span>
     </a>
   </div>
 
