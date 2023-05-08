9fa322e67894142ecaf2b08be6c9d54e094296ee
Nika Chitanava <chitanava@gmail.com>
2023-05-08 19:04:04 +0400

    decode HTML on biography page

--- resources/views/site/biography.blade.php
+++ resources/views/site/biography.blade.php
@@ -1,7 +1,7 @@
 <x-site.layout.app>
   <x-slot name="title">Biography</x-slot>
   
-  <div>
-    {!! $biography->body !!}
+  <div class="md:w-1/2 lg:w-1/3 text-justify trix-content">
+    {!! htmlspecialchars_decode($biography->body) !!}
   </div>
 </x-site.layout.app>
