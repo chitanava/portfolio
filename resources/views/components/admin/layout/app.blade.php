06de3980d2cdb2b9fac17c81dc6a91aedfadcc88
Nika Chitanava <chitanava@gmail.com>
2023-05-14 23:42:00 +0400

    add favicon

--- resources/views/components/admin/layout/app.blade.php
+++ resources/views/components/admin/layout/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
   <link href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap"
   rel="stylesheet">
   @vite(['resources/css/back.css', 'resources/js/back.js'])
--- resources/views/components/site/layout/app.blade.php
+++ resources/views/components/site/layout/app.blade.php
@@ -7,6 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="description" content="{{ $settings->seo_description }}">
   <meta name="keywords" content="{{ $settings->seo_keywords }}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
   <link
     href="https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap"
     rel="stylesheet">
@@ -27,7 +28,7 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col relative">
       <x-site.header/>
-      <main class="py-5 pl-10 pr-10 lg:pl-0 lg:py-10 space-y-5 lg:space-y-0">
+      <main class="pt-5 pb-10 pl-10 pr-10 lg:pl-0 lg:py-10 space-y-5 lg:space-y-0">
         {{ $slot }}
       </main>
     </div>
