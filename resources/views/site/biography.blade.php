5aad4605a9205e335193989a3999e944049d14e2
Nika Chitanava <chitanava@gmail.com>
2023-05-31 15:23:19 +0400

    open PDF links in new window on biography page

--- resources/views/site/biography.blade.php
+++ resources/views/site/biography.blade.php
@@ -1,7 +1,16 @@
 <x-site.layout.app>
   <x-slot name="title">{{ __('common.Biography') }}</x-slot>
-  
-  <div class="md:w-1/2 lg:w-1/3 text-justify trix-content">
+
+  <div class="lg:w-2/3 text-justify trix-content">
     {!! htmlspecialchars_decode($biography->body) !!}
   </div>
+
+  @push('footer-scripts')
+  <script>
+    const trixContentLinks = [...document.querySelectorAll('.trix-content a')];
+    trixContentLinks.forEach(function(el){
+      el.href.includes('.pdf') && el.setAttribute('target', '_blank');
+    })
+  </script>
+  @endpush
 </x-site.layout.app>
