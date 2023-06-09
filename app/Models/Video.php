21d4707f1d7a3bdcaf0d0948bf739529fbeb20e8
Nika Chitanava <chitanava@gmail.com>
2023-06-09 15:30:55 +0400

    add video

---
+++ app/Models/Video.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class Video extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+    use Sluggable;
+
+    protected $fillable = [
+        'title',
+        'url',
+        'caption',
+        'active',
+    ];
+
+    protected $appends = ['class'];
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('md')
+                ->width(600)
+                ->height(600);
+
+        $this->addMediaConversion('sm')
+                ->width(200)
+                ->height(200);
+    }
+
+    protected function class(): Attribute
+    {
+        return new Attribute(
+            get: fn () => Video::class,
+        );
+    }
+
+    public function videoable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
