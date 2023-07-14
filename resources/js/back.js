70df6843e3475d11c2e0cecdcf06dd776f349755
Nika Chitanava <chitanava@gmail.com>
2023-07-14 20:32:24 +0400

    remove file size from caption

--- resources/js/back.js
+++ resources/js/back.js
@@ -5,6 +5,7 @@ import Trix from "trix"
 document.addEventListener("trix-before-initialize", () => {
   Trix.config.attachments.preview.caption.name = false 
   Trix.config.attachments.preview.caption.size = false
+  Trix.config.attachments.file.caption.size = false
 })
 
  window.Alpine = Alpine
